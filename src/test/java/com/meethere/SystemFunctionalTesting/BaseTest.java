fde3d1b8fb12123ba174e04e26adbcd7ae769210
yanhhhhan <131138763+yanhhhhan@users.noreply.github.com>
2025-05-31 18:18:14 +0800

    Update BaseTest.java

--- src/test/java/com/meethere/SystemFunctionalTesting/BaseTest.java
+++ src/test/java/com/meethere/SystemFunctionalTesting/BaseTest.java
@@ -15,7 +15,7 @@ public class BaseTest {
     JavascriptExecutor js;
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "F:\\JetBrains\\IntelliJ IDEA 2019.1.3\\SeleniumWebDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\chrome\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
